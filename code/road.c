30a78efc0f0a17ff5f87f41c5739caae45da10fd
Nick Foley <nickmfoley@gmail.com>
2023-11-13 22:36:31 -0700

    Started moveCars function in road.c

--- code/road.c
+++ code/road.c
@@ -20,6 +20,19 @@ void freeRoad(RoadData* road){
 
 /* Suggested TODO - Write a function that attempts to move the cars on a given road forward (if space ahead of them was empty).
 */
+void moveCars(RoadData* road, int k/*k is the current position the car is in*/) {
+ if (road->cars[k] != NULL && road->cars[k + 1] == NULL) {
+  road->cars[k + 1] = road->cars[k]; // moves the car forward
+  road->cars[k] = NULL; // sets "current" slot to NULL
+ }else if (road->cars[road->roadlen - 1]) {
+ /* this should check if car is at the end of the road.
+   probably call the function that changes intersections.
+  */
+ }else {
+  /* car cannot move forward */
+ }
+
+}
 
 /* Suggested TODO - Write a function to attempt to move the car at the first position on a given road
  * to the last position of a another given road.
