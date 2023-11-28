7843317d76bf69becf5ccf7d86026eb103817e54
bichttor <129998286+bichttor@users.noreply.github.com>
2023-11-27 18:40:04 -0600

    Update road.c
    added functions , moved cars well, need to implement light function into the move functions

--- code/road.c
+++ code/road.c
@@ -15,72 +15,93 @@ RoadData* createRoad(RoadData* road, int from, int roadlen, int green, int red, 
 /* Suggested TODO - Write a function to free a road.
  */
 void freeRoad(RoadData* road){
-    /*create loop that frees all cars left in road*/
-
+    int i,j;
+    for(i = 0; i < road->numRoad; i++){/*goes through all the roads*/
+      for(j = 0; j < road[i].roadlen; j++){/*free all cars left in road*/
+        if(road[i].cars[j] != NULL){
+          freeCar(road[i].cars[j]); 
+        }
+      }
+      
+      freeQueue(road[i].q);
+      free(road[i].cars);
+    }
     free(road);
+    
 }
 
 /* Suggested TODO - Write a function that attempts to move the cars on a given road forward (if space ahead of them was empty).
 */
-void moveCars(RoadData* road, int k/*k is the current position the car is in*/) {
-    /* Car position might be passed from Event*/
-    if (road->cars[k] != NULL && road->cars[k + 1] == NULL) {
-        road->cars[k + 1] = road->cars[k]; // moves the car forward
-        road->cars[k] = NULL; // sets "current" slot to NULL
+void moveCars(RoadData* road) {
+    int i,j;
+      for(i = 0; i < road->numRoad; i++){/*goes through all the roads*/
+        for(j = road[i].roadlen-1; j >= 0; j--){/*starts from the "end" of the road*/
+          if (road[i].cars[j] != NULL && road[i].cars[j - 1] == NULL && road[i].cars[j]->moved == true){/*checks to see if position contains car and the next position is NULL and car hasnt moved*/
+            road[i].cars[j]->moved = false;/*has moved*/
+            road[i].cars[j - 1] = road[i].cars[j];
+            road[i].cars[j] = NULL;  
+          }
         }
-    else if (road->cars[road->roadlen-1]/*if true we should be at the end of the array*/) {
-        roadHopper(road->from, road->to);
-        }
-    else if (road->cars[k -1] != NULL && road->cars[k + 1] != NULL) {
-        return;
-        }
-
-    }
+      }
+}
 
 /* Suggested TODO - Write a function to attempt to move the car at the first position on a given road
  * to the last position of a another given road.
  */
-void roadHopper(Queue* leavingThisQueue, RoadData* road) {
-    // This will not work, we need to read in just RoadData* and figure out a way to get the from -> to road queues
-    // Maybe road->from and road->to will work?
-
-    if (front(leavingThisQueue) == NULL) {
-        return;
+void roadHopper(RoadData* road) {
+   int i;
+    for(i = 0; i < road->numRoad; i++){
+      if(road[i].cars[0] != NULL){
+        /*maybe get shortest distance to find next node then enqueue the car into that node, if light is green
+        and the spot is NULL then add car to the road else leave in the queue*/
+      }
     }
-    // Should we update the Car** array here too?
-
-    Car* car = dequeue(leavingThisQueue);
-    enqueue(road->q, car);
 }
 
 /* Suggested TODO - Write a function to attempt to move a car from the add car queue onto the end of this road.
  */
 void queueToRoad(Queue* leavingThisQueue, RoadData* road) {
-    if (front(leavingThisQueue) == NULL || road->cars[0] != NULL) {
-        return;
-    }
-    // Could add error handling here
-    // Should we update the Car** array here too?
-    Car* car = dequeue(leavingThisQueue);
-    enqueue(road->q, car);
+      int i;
+      queueType qt; /*car*/
+      for(i = 0; i < road->numRoad; i++){ 
+        if(road[i].cars[road[i].roadlen-1] == NULL && !isEmpty(road[i].q)){/*checks if "end" and queue are valid , dequeues from queue and places are at the "end"*/
+          qt = dequeue(road[i].q);
+          road[i].cars[road[i].roadlen-1] = qt; 
+          qt->moved = false;/*has moved*/
+        }
+      }
+      
 }
 
 
 /* Suggested TODO - Write a function to update the current state of the traffic light based on the current cycle.
  */
 void updateLight(RoadData* road) {
-  int i;
+ int i;
   for(i = 0; i < road->numRoad;i++){
-    if(road[i].lightcounter%road[i].reset == road[i].green){
+    if(j%road[i].reset == road[i].green){
     road[i].light = true;
     }
-    if(road[i].lightcounter%road[i].reset == road[i].red){
+    if(j%road[i].reset == road[i].red){
     road[i].light = false;
       }
-    road[i].lightcounter++;
     }
  
 }
+
+/*resets moves for next timestep*/
+void resetCar(RoadData* road ){
+  int i,j;
+  for(i = 0; i < road->numRoad; i++){/*goes through all the roads*/
+    for(j = 0; j < road[i].roadlen; j++){/*free all cars left in road*/
+      if(road[i].cars[j] != NULL){
+        road[i].cars[j]->moved = true;/*hasnt moved*/
+      }
+    }
+  }
+  
+}
+
 /* Suggested TODO - Write a function to print the destinations of the cars on the road
  */
 void printDestinations(RoadData* road, int j) {
@@ -107,6 +128,12 @@ void printRoadsEvent(RoadData* road, int j){
        else{
          printf("- ");
        }
+     }
+      if(road[i].light == true){
+       printf("(GREEN LIGHT)");
+     }
+     else{
+       printf("(RED LIGHT)");
      }
   }
   printf("\n");
